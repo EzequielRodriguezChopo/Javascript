441cb1738376f31f1166e2348762d1dca4ecf013
Ezequiel Rodriguez <ezequielrodriguezchopo@gmail.com>
2022-06-06 01:05:24 -0300

    Desafio Interactuar con HTML

--- js/main.js
+++ js/main.js
@@ -1,163 +1,44 @@
-class Productos {
-  constructor(nombreProducto, stock, precio, esSinTacc) {
-    this.nombreProducto = nombreProducto;
-    this.stock = stock;
-    this.precio = precio;
-    this.esSinTacc = esSinTacc;
-  }
-}
-class Usuarios {
-  constructor(nombre, apellido, edad, email) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.edad = edad;
-    this.email = email;
-  }
-}
-
-let primerLetraMayuscula = (user) => {
-  return user.charAt(0).toUpperCase() + user.slice(1);
-};
-
-let sumar = () => {
-  sumaDeLaCompra = 0;
-  carrito.forEach((productoComprados)=>{
-    sumaDeLaCompra = productoComprados.precio + sumaDeLaCompra;
-  })
-  return sumaDeLaCompra
-}
-
-let productosAdquiridos = ()=>{
-  listaProductosAdquiridos = carrito.map((productos)=>{
-    return productos.nombreProducto;
-  })
-  return listaProductosAdquiridos
-}
-
-let producto1 = new Productos("medialuna", 16, 150, false);
-let producto2 = new Productos("bizcocho", 35, 150, false);
-let producto3 = new Productos("vigilante", 8, 250, false);
-let producto4 = new Productos("alfajor", 40, 30, true);
-let producto5 = new Productos("torta", 5, 1050, true);
-let producto6 = new Productos("cremona", 12, 350, true);
-
-let usuario1 = new Usuarios("ezequiel","rodriguez",34,"ezequiel.rodriguez@coderhoouse");
-let usuario2 = new Usuarios("gonzalo","manoukian",31,"gonzalo.manoukian@coderhoouse");
-let usuario3 = new Usuarios("nicolas","villaverde",34,"nicolas.villaverde@coderhoouse");
-let usuario4 = new Usuarios("chopo","senda",34,"chopo.senda@coderhoouse");
-let usuario5 = new Usuarios("camilo","lindarte",34,"camilo.lindarte@coderhoouse");
-let usuario6 = new Usuarios("cristian","cuello",34,"cristian.cuello@coderhoouse");
-
-let baseDatosUsuario = [
-  usuario1.nombre,
-  usuario2.nombre,
-  usuario3.nombre,
-  usuario4.nombre,
-  usuario5.nombre,
-  usuario6.nombre,
-];
-let opcionComprar;
-let usuario;
-let checkeoUsuario;
-let productoAComprar;
-let carrito = [];
-let sumaDeLaCompra;
-let listaProductosAdquiridos;
-let op
-
-opcionComprar = prompt("Desea comprar alguna factura? Si/No").toLowerCase();        //Ingresamos opcion de compra
-while (opcionComprar != "si" && opcionComprar != "no") {
-  opcionComprar = prompt("Ingrese una de estas opciones Si/No").toLowerCase();      //Que ingrese si o no
-}
-if(opcionComprar=='si'){
-  do{
-    usuario = prompt("Ingrese su nombre por favor").toLowerCase();                    //Comienza verificacion de usuario
-    checkeoUsuario=baseDatosUsuario.some((comprador)=>comprador==usuario);            //Terminamos la verificacion de usuario
-    if (checkeoUsuario==true){
-      alert('Bienvenido ' + primerLetraMayuscula(usuario));
-      op='no';
-    } else {
-      alert('Usted no esta registado en nuestra base de datos');
-      op = prompt('Desea ingresar su usuario de nuevo? Si/No').toLowerCase();
+class Productos{
+    constructor(nombreProducto,imagenProducto,precioProducto){
+        this.nombreProducto=nombreProducto;
+        this.imagenProducto=imagenProducto;
+        this.precioProducto=precioProducto
     }
-  }while(op=='si')
-  while (opcionComprar == "si" && checkeoUsuario == true) {                         //Si existe el usuario y quiere comprar puede acceder
-
-    productoAComprar = prompt("Ingrese el producto que quiere comprar: Medialuna/Bizcocho/Vigilante/Alfajor/Torta/Cremona").toLowerCase();
-    switch (productoAComprar) {
-      case "medialuna":
-        if(producto1.stock==-1){
-          alert('No poseemos más stock del producto seleccionado');
-          break;
-        }
-        alert("Usted selecciono Medialuna, que tienen un costo de: " + producto1.precio + ", quedan en stock: " + producto1.stock);
-        producto1.stock=producto1.stock-1;                                        // Resto 1 al stock actual
-        carrito.push(producto1);                                                  // Agrego un articulo a mi carrito
-        break;
-      case "bizcocho":
-        if(producto2.stock==-1){
-          alert('No poseemos más stock del producto seleccionado');
-          break;
-        }
-        alert("Usted selecciono Bizcochos, que tienen un costo de: " + producto2.precio + ", quedan en stock: " + producto2.stock);
-        producto2.stock=producto2.stock-1;                                        // Resto 1 al stock actual
-        carrito.push(producto2);                                                  // Agrego un articulo a mi carrito
-        break;
-      case "vigilante":
-        if(producto3.stock==-1){
-          alert('No poseemos más stock del producto seleccionado');
-          break;
-        }
-        alert("Usted selecciono Vigilantes, que tienen un costo de: " + producto3.precio + ", quedan en stock: " + producto3.stock);
-        producto3.stock=producto3.stock-1;                                        // Resto 1 al stock actual
-        carrito.push(producto3);                                                  // Agrego un articulo a mi carrito
-        break;
-      case "alfajor":
-        if(producto4.stock==-1){
-          alert('No poseemos más stock del producto seleccionado');
-          break;
-        }
-        alert("Usted selecciono Alfajores, que tienen un costo de: " + producto4.precio + ", quedan en stock: " + producto4.stock);
-        producto4.stock=producto4.stock-1;                                        // Resto 1 al stock actual
-        carrito.push(producto4);                                                  // Agrego un articulo a mi carrito
-        break;
-      case "torta":
-        if(producto5.stock==-1){
-          alert('No poseemos más stock del producto seleccionado');
-          break;
-        }
-        alert("Usted selecciono Torta, que tienen un costo de: " + producto5.precio + ", quedan en stock: " + producto5.stock);
-        producto5.stock=producto5.stock-1;                                        // Resto 1 al stock actual
-        carrito.push(producto5);                                                  // Agrego un articulo a mi carrito
-        break;
-      case "cremona":
-        if(producto6.stock==-1){
-          alert('No poseemos más stock del producto seleccionado');
-          break;
-        }
-        alert("Usted selecciono Cremona, que tienen un costo de: " + producto6.precio + ", quedan en stock: " + producto6.stock);
-        producto6.stock=producto6.stock-1;                                        // Resto 1 al stock actual
-        carrito.push(producto6);                                                  // Agrego un articulo a mi carrito
-        break;
-      default:
-        alert("No poseemos el producto seleccionado");
-        break;
-    }
-    opcionComprar = prompt("Desea comprar alguna factura? Si/No").toLowerCase();
-    while (opcionComprar != "si" && opcionComprar != "no") {
-      opcionComprar = prompt("Ingrese una de estas opciones Si/No").toLowerCase();
-    }
-  }
 }
+const producto1 = new Productos('Trota de Ricota',"../imagenes/tortaDeRicota.jpg",900);
+const producto2 = new Productos('Galletitas',"../imagenes/galletas.jpg",200);
+const producto3 = new Productos('Chipa',"../imagenes/chipa.jpeg",250);
+const producto4 = new Productos('Cremona',"../imagenes/cremona.jpeg" ,150);
+const producto5 = new Productos('Biscochos de Grasa',"../imagenes/bizcochos.jpeg",125);
+const producto6 = new Productos('Facturas',"../imagenes/facturas.jpg",400);
+const producto7 = new Productos('Sanguches de Miga',"../imagenes/sanguchesDeMiga.jpeg",60);
+const producto8 = new Productos('Pasta Frola',"../imagenes/pastafrola.jpg",900);
+const producto9 = new Productos('Tarta de Coco',"../imagenes/tartaDeCoco.jpg",900);
 
-carrito=carrito.map((prod)=>{
-  return {nombreProducto:primerLetraMayuscula(prod.nombreProducto),precio:prod.precio}
+const productos = [producto1,producto2,producto3,producto4,producto5,producto6,producto7,producto8,producto9];
+
+const productosHTML = document.getElementById('productos');
+
+productos.forEach((elemento)=>{
+    const item=document.createElement('div') ;
+    item.className = 'col';
+    item.innerHTML=`
+    <div class="card mb-4 rounded-3 shadow-sm producto bordeProducto">
+      <div class="card-header py-3 ">
+        <h4 class="my-0 ">${elemento.nombreProducto}</h4>
+      </div>
+      <div class="">
+        <img src=${elemento.imagenProducto} alt="Cremona" class="articulo">
+        <ul class="list-unstyled mt-3 mb-4">
+          <h4>Precio x Un $${elemento.precioProducto}</h4>
+          <div class="carrito">
+            <a href="../paginas/productos.html" class="carrito-link botonCarrito">
+              Agregar al Carrito
+            </a>
+          </div>
+        </ul>
+      </div>
+    </div>
+    `;
+    productosHTML.append(item);
 })
-
-console.log(carrito);
-
-if(carrito==''){
-  alert('Que tenga usted un buen día')
-}else{
-  alert("Que tengas un buen día, usted ha comprado " + productosAdquiridos() + ', con un precio total de $'+ sumar());
-}
